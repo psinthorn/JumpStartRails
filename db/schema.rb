18f43547f9ca15f93beb0a67071e4f84e296805e
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-02-18 15:32:58 +0700

    Implement devise authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_063813) do
+ActiveRecord::Schema.define(version: 2020_02_18_071511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   create_table "welcomes", force: :cascade do |t|
     t.string "title"
@@ -23,6 +36,9 @@ ActiveRecord::Schema.define(version: 2020_02_18_063813) do
     t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_welcomes_on_user_id"
   end
 
+  add_foreign_key "welcomes", "users"
 end
